e01887854aff297fd130954b814fbeed2b53ae95
zhou1178539345 <1178539345@qq.com>
2017-04-11 23:58:09 +0800

    goods添加banner和分类排行

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -1,13 +1,39 @@
 <template>
-    <div>
-
+    <div class="goods publiccss">
+      <publicheader :menushow="menushow" :headtitle="headtitle" ></publicheader>
+      <swiper></swiper>
+      <mu-flexbox :gutter="0">
+        <mu-flexbox-item class="flexitem">
+          1
+        </mu-flexbox-item>
+        <mu-flexbox-item class="flexitem" gutter="0">
+          2
+        </mu-flexbox-item>
+      </mu-flexbox>
     </div>
 </template>
 <style lang="less">
-
+  @import './../assets/css/public.css';
+  .flexitem{
+    background-color: #565645;
+    text-align: center;
+    height: 60px;
+    line-height: 60px;
+  }
 </style>
 <script type="text/ecmascript-6">
+  import publicheader from './public/publicHeader'
+  import swiper from './public/swiper'
     export default{
-
+      data() {
+        return {
+          menushow: true,
+          headtitle: "干货",
+        }
+      },
+      components: {
+        publicheader,
+        swiper
+      }
     }
 </script>
