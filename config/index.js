7f4f61326d325c389f68b0fb764b83e13d1d6e85
zhou1178539345 <1178539345@qq.com>
2017-07-06 21:11:20 +0800

    docs

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       // '/api': {
       //   target: 'https://api.leancloud.cn:443/1.1/',
